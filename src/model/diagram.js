9b89066d7ec359d3b658fc976f004adbb08dc954
Roman Masek <romario333@gmail.com>
2012-06-29 09:29:23 +0200

    sync

--- src/model/diagram.js
+++ src/model/diagram.js
@@ -14,154 +14,159 @@ define(function(require) {
      * FIXME: Note that to track changes within individual items, you have to use their observer interface.
      *
      */
-    return function () {
+    function Diagram() {
+        this._items = [];
+        this._onItemAddedHandlers = [];
+        this._onItemRemovedHandlers = [];
+        this._changeEventsEnabled = true;
+        this._lastId = 0;
+    }
 
-        var that = {},
-            items = [],
-            onItemAddedHandlers = [],
-            onItemRemovedHandlers = [],
-            changeEventsEnabled = true,
-            lastId = 0;
+    /**
+     * Adds item to the diagram and assigns it an `id`.
+     *
+     * @param item
+     */
+    Diagram.prototype.addItem = function (item) {
+        if (!item.isLine && !item.isNode) {
+            throw new Error("Only nodes and lines can be added to diagram");
+        }
 
-        /**
-         * Adds item to the diagram and assigns it an `id`.
-         *
-         * @param item
-         */
-        that.addItem = function (item) {
-            if (!item.isLine && !item.isNode) {
-                throw new Error("Only nodes and lines can be added to diagram");
-            }
+        item.id = ++this._lastId;
 
-            item.id = ++lastId;
-
-            if (item.isLine) {
-                item.points().forEach(function (point) { // TODO: put to line via some generic callback?
-                    point.id = ++lastId;
-                });
-            }
-
-            items.push(item);
-            if (changeEventsEnabled) {
-                that.fireItemAdded(item);
-            }
-        };
-
-        /**
-         * Removes item from the diagram.
-         *
-         * @param item
-         */
-        that.removeItem = function (item) {
-            if (!item.isLine && !item.isNode) {
-                throw new Error("Only nodes and lines can be removed from diagram");
-            }
-
-            var i = items.indexOf(item);
-            if (i === -1) {
-                throw new Error("Item '{itemId}' not found in diagram.".supplant({itemId: item.id}));
-            }
-
-            // remove item's connection before removing it from diagram
-            item.disconnect();
-
-            items.splice(i, 1);
-            item.id = null;
-
-            if (item.isLine) {
-                item.points().forEach(function (point) {
-                    point.id = null;
-                });
-            }
-
-            if (changeEventsEnabled) {
-                that.fireItemRemoved(item);
-            }
-        };
-
-        /**
-         * Returns all items in the diagram.
-         *
-         * @return {Array}
-         */
-        that.items = function () {
-            return items;
-        };
-
-        /**
-         * This function allows you to enable or disable firing of change events.
-         * If no `value` is provided, it returns whether change events are enabled.
-         *
-         * @param value
-         * @return {Boolean}
-         */
-        that.changeEventsEnabled = function (value) {
-            if (arguments.length === 0) {
-                return changeEventsEnabled;
-            } else {
-                changeEventsEnabled = value;
-            }
-        };
-
-        /**
-         * Binds an event handler to the `itemAdded` event.
-         *
-         * @param handler   Function to call when an item is added to the diagram.
-         */
-        that.itemAdded = function (handler) {
-            onItemAddedHandlers.push(handler);
-        };
-
-        /**
-         * Binds an event handler to the `itemRemoved` event.
-         *
-         * @param handler   Function to call when an item is removed from the diagram.
-         */
-        that.itemRemoved = function (handler) {
-            onItemRemovedHandlers.push(handler);
-        };
-
-        /**
-         * Fires the `itemAdded` event. You want to use this typically when you have disabled change events to do several
-         * changes at once and now want to notify observers about these changes.
-         *
-         * @param item  Item which has been added to the diagram.
-         */
-        that.fireItemAdded = function (item) {
-            onItemAddedHandlers.forEach(function (handler) {
-                handler(that, item);
+        if (item.isLine) {
+            var that = this;
+            item.points().forEach(function (point) { // TODO: put to line via some generic callback?
+                point.id = ++that._lastId;
             });
-        };
+        }
 
-        /**
-         * Fires the `itemRemoved` event. You want to use this typically when you have disabled change events to do several
-         * changes at once and now want to notify observers about these changes.
-         *
-         * @param item  Item which has been removed from the diagram.
-         */
-        that.fireItemRemoved = function (item) {
-            onItemRemovedHandlers.forEach(function (handler) {
-                handler(that, item);
-            });
-        };
-
-        /**
-         * Returns JSON representation of the diagram.
-         *
-         * @return {String}
-         */
-        that.toJSON = function () {
-            var json = "[";
-            items.forEach(function (item, index) {
-                json += item.toJSON();
-                if (index < items.length - 1) {
-                    json += ", ";
-                }
-            });
-            json += "]";
-            return json;
-        };
-
-        return that;
+        this._items.push(item);
+        if (this._changeEventsEnabled) {
+            this.fireItemAdded(item);
+        }
     };
+
+    /**
+     * Removes item from the diagram.
+     *
+     * @param item
+     */
+    Diagram.prototype.removeItem = function (item) {
+        if (!item.isLine && !item.isNode) {
+            throw new Error("Only nodes and lines can be removed from diagram");
+        }
+
+        var i = this._items.indexOf(item);
+        if (i === -1) {
+            throw new Error("Item '{itemId}' not found in diagram.".supplant({itemId: item.id}));
+        }
+
+        // remove item's connection before removing it from diagram
+        item.disconnect();
+
+        this._items.splice(i, 1);
+        item.id = null;
+
+        if (item.isLine) {
+            item.points().forEach(function (point) {
+                point.id = null;
+            });
+        }
+
+        if (this._changeEventsEnabled) {
+            this.fireItemRemoved(item);
+        }
+    };
+
+    /**
+     * Returns all items in the diagram.
+     *
+     * @return {Array}
+     */
+    Diagram.prototype.items = function () {
+        return this._items;
+    };
+
+    // TODO: proc fce? ted uz vypada trochu zbytecne
+    /**
+     * This function allows you to enable or disable firing of change events.
+     * If no `value` is provided, it returns whether change events are enabled.
+     *
+     * @param value
+     * @return {Boolean}
+     */
+    Diagram.prototype.changeEventsEnabled = function (value) {
+        if (arguments.length === 0) {
+            return this._changeEventsEnabled;
+        } else {
+            this._changeEventsEnabled = value;
+        }
+    };
+
+    /**
+     * Binds an event handler to the `itemAdded` event.
+     *
+     * @param handler   Function to call when an item is added to the diagram.
+     */
+    Diagram.prototype.itemAdded = function (handler) {
+        this._onItemAddedHandlers.push(handler);
+    };
+
+    /**
+     * Binds an event handler to the `itemRemoved` event.
+     *
+     * @param handler   Function to call when an item is removed from the diagram.
+     */
+    Diagram.prototype.itemRemoved = function (handler) {
+        this._onItemRemovedHandlers.push(handler);
+    };
+
+    /**
+     * Fires the `itemAdded` event. You want to use this typically when you have disabled change events to do several
+     * changes at once and now want to notify observers about these changes.
+     *
+     * @param item  Item which has been added to the diagram.
+     */
+    Diagram.prototype.fireItemAdded = function (item) {
+        var that = this;
+        this._onItemAddedHandlers.forEach(function (handler) {
+            handler(that, item);
+        });
+    };
+
+    /**
+     * Fires the `itemRemoved` event. You want to use this typically when you have disabled change events to do several
+     * changes at once and now want to notify observers about these changes.
+     *
+     * @param item  Item which has been removed from the diagram.
+     */
+    Diagram.prototype.fireItemRemoved = function (item) {
+        var that = this;
+        this._onItemRemovedHandlers.forEach(function (handler) {
+            handler(that, item);
+        });
+    };
+
+    /**
+     * Returns JSON representation of the diagram.
+     *
+     * @return {String}
+     */
+    Diagram.prototype.toJSON = function () {
+        var json = "[";
+        var that = this;
+        this._items.forEach(function (item, index) {
+            json += item.toJSON();
+            if (index < that._items.length - 1) {
+                json += ", ";
+            }
+        });
+        json += "]";
+        return json;
+    };
+
+    return function () {
+        return new Diagram();
+    }
 });
