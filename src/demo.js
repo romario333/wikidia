3a9ec3de0a45ae557e4f0eaefb67ed4a4034bae5
Roman Masek <romario333@gmail.com>
2012-09-27 14:25:18 +0200

    Return the warning, experience in other browsers still not great :(

--- src/demo.js
+++ src/demo.js
@@ -1,6 +1,10 @@
 define(function(require, exports, module) {
     "use strict";
 
+    if (!$.browser.webkit) {
+        alert("Use Google Chrome or Safari if possible. I haven't time to make this work properly on other browsers. Sorry about that :(")
+    }
+
     var model = require("model");
     var view = require("view");
     var diagramPresenter = require("presenter/diagramPresenter");
