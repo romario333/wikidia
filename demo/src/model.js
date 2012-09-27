2b64a7e410a44eb8638c4cb82c3428a1e24741d5
Roman Masek <romario333@gmail.com>
2012-09-27 14:20:11 +0200

    Delete unoptimized demo

--- demo/src/model.js
+++
@@ -1,9 +0,0 @@
-define(function(require) {
-    "use strict";
-
-    return {
-        diagram: require("model/diagram"),
-        line: require("model/line"),
-        node: require("model/node")
-    };
-});
